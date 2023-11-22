cb866b5492a64533ac40c44c3b772994c6b2cedf
YUCHEN005 <YUCHEN005@e.ntu.edu.sg>
2023-11-22 17:16:28 +0800

    Upload H2T-LoRA code

---
+++ H2T-LoRA/README.md
@@ -0,0 +1,35 @@
+# H2T-LoRA Training
+## Prompt Template ##
+Please see the template in templates/H2T-LoRA.json
+
+You can modify it using your own task description.
+
+## Path ##
+--data_path: Your data path includes Hypo-Trans pairs.
+
+--output_dir: The path to save LoRA weight
+
+## Other Hyperparameters ##
+
+--base_model: The foundation model for H2T learning
+
+--lora_r: LoRA rank
+
+--lora_target_modules: The modules to apply the LoRA update matrices, e.g., ["q_proj", "k_proj", "v_proj", "o_proj"], 
+
+--val_set_size: The validation set size from training set.
+
+## Usage ##
+
+python finetune.py \
+    --base_model 'yahma/llama-7b-hf' \
+    --data_path './data/train_wsj.json' \
+    --output_dir './wsj' \
+    --lora_target_modules='[q_proj,k_proj, v_proj, o_proj]' \
+    --num_epochs=10 \
+    --cutoff_len=512 \
+    --group_by_length \
+    --learning_rate 2e-4 \
+    --micro_batch_size=64 \
+    --batch_size=256 \
+    --lora_r=16 
---
+++ H2T-LoRA/utils/prompter.py
@@ -0,0 +1,50 @@
+"""
+A dedicated helper to manage templates and prompt building.
+"""
+
+import json
+import os.path as osp
+from typing import Union
+
+
+class Prompter(object):
+    __slots__ = ("template", "_verbose")
+
+    def __init__(self, template_name: str = "", verbose: bool = False):
+        self._verbose = verbose
+        if not template_name:
+            # Enforce the default here, so the constructor can be called with '' and will not break.
+            template_name = "H2T-LoRA"
+        file_name = osp.join("templates", f"{template_name}.json")
+        if not osp.exists(file_name):
+            raise ValueError(f"Can't read {file_name}")
+        with open(file_name) as fp:
+            self.template = json.load(fp)
+        if self._verbose:
+            print(
+                f"Using prompt template {template_name}: {self.template['description']}"
+            )
+
+    def generate_prompt(
+        self,
+        input: Union[None, str] = None,
+        label: Union[None, str] = None,
+        input2: Union[None, str] = None,
+    ) -> str:
+        # returns the full prompt from instruction and optional input
+        # if a label (=response, =output) is provided, it's also appended.
+        if input2 is None:
+            res = self.template["prompt_h2t_1best"].format(
+                     input=input)
+        if input2 is not None:
+            res = self.template["prompt_h2t_nbest"].format(
+                best=input, others=input2)
+
+        if label:
+            res = f"{res}{label}"
+        if self._verbose:
+            print(res)
+        return res
+
+    def get_response(self, output: str) -> str:
+        return output.split(self.template["response_split"])[1].strip()
